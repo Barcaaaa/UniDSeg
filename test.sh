0799f8376ba43bab125839ebd464cbd421e6d412
Barcaaaa <43379973+Barcaaaa@users.noreply.github.com>
2024-05-28 18:43:02 +0800

    anonymous check

--- test.sh
+++ test.sh
@@ -38,25 +38,25 @@
 ### vkitti/skitti
 #CUDA_VISIBLE_DEVICES=0 python -W ignore xmuda/test_ft_clip.py \
 #    --cfg=configs/vkitti_skitti/baseline_dg.yaml \
-#    --ckpt2d=/data1/wuyao/code/UniDSeg/output/vkitti_skitti/dg/UniDSeg_ViT-L-14/model_2d_027000.pth \
-#    --ckpt3d=/data1/wuyao/code/UniDSeg/output/vkitti_skitti/dg/UniDSeg_ViT-L-14/model_3d_024000.pth \
+#    --ckpt2d=/data1/user1/code/UniDSeg/output/vkitti_skitti/dg/UniDSeg_ViT-L-14/model_2d_027000.pth \
+#    --ckpt3d=/data1/user1/code/UniDSeg/output/vkitti_skitti/dg/UniDSeg_ViT-L-14/model_3d_024000.pth \
 #CUDA_VISIBLE_DEVICES=3 python -W ignore xmuda/test_ft_clip.py \
 #    --cfg=configs/vkitti_skitti/baseline_da.yaml \
-#    --ckpt2d=/data1/wuyao/code/UniDSeg/output/vkitti_skitti/da/UniDSeg_ViT-L-14/model_2d_036000.pth \
-#    --ckpt3d=/data1/wuyao/code/UniDSeg/output/vkitti_skitti/da/UniDSeg_ViT-L-14/model_3d_060000.pth \
+#    --ckpt2d=/data1/user1/code/UniDSeg/output/vkitti_skitti/da/UniDSeg_ViT-L-14/model_2d_036000.pth \
+#    --ckpt3d=/data1/user1/code/UniDSeg/output/vkitti_skitti/da/UniDSeg_ViT-L-14/model_3d_060000.pth \
 
 
 ### a2d2/skitti
 #CUDA_VISIBLE_DEVICES=0 python -W ignore xmuda/test_ft_clip.py \
 #    --cfg=configs/a2d2_skitti/baseline_dg.yaml \
-#    --ckpt2d=/data1/wuyao/code/UniDSeg/output/a2d2_skitti/dg/UniDSeg_ViT-L-14/model_2d_105000.pth \
-#    --ckpt3d=/data1/wuyao/code/UniDSeg/output/a2d2_skitti/dg/UniDSeg_ViT-L-14/model_3d_150000.pth \
+#    --ckpt2d=/data1/user1/code/UniDSeg/output/a2d2_skitti/dg/UniDSeg_ViT-L-14/model_2d_105000.pth \
+#    --ckpt3d=/data1/user1/code/UniDSeg/output/a2d2_skitti/dg/UniDSeg_ViT-L-14/model_3d_150000.pth \
 #CUDA_VISIBLE_DEVICES=0 python -W ignore xmuda/test_ft_clip.py \
 #    --cfg=configs/a2d2_skitti/baseline_da.yaml \
-#    --ckpt2d=/data1/wuyao/code/UniDSeg/output/a2d2_skitti/da/UniDSeg_ViT-L-14/model_2d_082500.pth \
-#    --ckpt3d=/data1/wuyao/code/UniDSeg/output/a2d2_skitti/da/UniDSeg_ViT-L-14/model_3d_135000.pth \
+#    --ckpt2d=/data1/user1/code/UniDSeg/output/a2d2_skitti/da/UniDSeg_ViT-L-14/model_2d_082500.pth \
+#    --ckpt3d=/data1/user1/code/UniDSeg/output/a2d2_skitti/da/UniDSeg_ViT-L-14/model_3d_135000.pth \
 #CUDA_VISIBLE_DEVICES=3 python -W ignore xmuda/test_ft_clip.py \
 #    --cfg=configs/a2d2_skitti/baseline_da_pl.yaml \
-#    --ckpt2d=/data1/wuyao/code/UniDSeg/output/a2d2_skitti/sfda/UniDSeg_ViT-L-14/model_2d_022500.pth \
-#    --ckpt3d=/data1/wuyao/code/UniDSeg/output/a2d2_skitti/sfda/UniDSeg_ViT-L-14/model_3d_060000.pth \
+#    --ckpt2d=/data1/user1/code/UniDSeg/output/a2d2_skitti/sfda/UniDSeg_ViT-L-14/model_2d_022500.pth \
+#    --ckpt3d=/data1/user1/code/UniDSeg/output/a2d2_skitti/sfda/UniDSeg_ViT-L-14/model_3d_060000.pth \
 
